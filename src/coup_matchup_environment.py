e7b983743bd11067030fd2f97f7cf19219bf3656
Matthew Cohen <cohensmatthew@gmail.com>
2023-11-15 21:11:28 -0500

    changed dunders
    added dunders to winning region class variables and changed double dunder to single dunder for solve_winning_region

--- src/coup_matchup_environment.py
+++ src/coup_matchup_environment.py
@@ -1,8 +1,8 @@
 class CoupMatchupEnvironment:
     def __init__(self, initial_state):
         self.initial_state = initial_state
-        self.player_1_winning_region = None
-        self.player_2_winning_region = None
+        self._player_1_winning_region = None
+        self._player_2_winning_region = None
 
     def get_states(self):
         """
@@ -22,7 +22,7 @@ class CoupMatchupEnvironment:
         """
         pass
 
-    def __solve_winning_region(self, player):
+    def _solve_winning_region(self, player):
         """
         Calculates and returns the winning region of player
         :param player: An integer representing the player who's winning region should be returned
@@ -34,8 +34,8 @@ class CoupMatchupEnvironment:
         """
         Calculate the winning regions of both players and store them in the winning region class variables
         """
-        self.player_1_winning_region = self.__solve_winning_region(player=1)
-        self.player_2_winning_region = self.__solve_winning_region(player=2)
+        self._player_1_winning_region = self._solve_winning_region(player=1)
+        self._player_2_winning_region = self._solve_winning_region(player=2)
 
     def get_win_region(self, player):
         """
@@ -44,10 +44,10 @@ class CoupMatchupEnvironment:
         :return: The stored list of winning states for player
         """
         if player == 1:
-            assert self.player_1_winning_region is not None, f"Player 1 winning region not defined for {self}"
-            return self.player_1_winning_region
+            assert self._player_1_winning_region is not None, f"Player 1 winning region not defined for {self}"
+            return self._player_1_winning_region
         elif player == 2:
-            assert self.player_2_winning_region is not None, f"Player 2 winning region not defined for {self}"
-            return self.player_2_winning_region
+            assert self._player_2_winning_region is not None, f"Player 2 winning region not defined for {self}"
+            return self._player_2_winning_region
         else:
             raise Exception(f"Player {player} not defined in {self}")
--- src/run_experiment.py
+++ src/run_experiment.py
@@ -2,6 +2,7 @@ from coup_matchup_environment import CoupMatchupEnvironment
 def main():
     initial_state = ("state", "values", "here")
     matchup = CoupMatchupEnvironment(initial_state)
+    matchup.solve()
     print(f"Player {1 if initial_state in matchup.get_win_region(1) else 2} wins")
 
 if __name__ == "__main__":
