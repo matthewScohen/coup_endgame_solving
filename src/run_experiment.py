1217d8fc54fc71d243c74e11d566158a76537387
Matthew Cohen <cohen.matthew@ufl.edu>
2023-11-28 21:25:04 -0500

    implement run_experiment function
    run_experiment now evaluates all matchups and writes the winners to a file

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,5 +1,8 @@
-import constants
+import itertools
 import random
+import csv
+
+import constants
 from coup_matchup_environment import CoupMatchupEnvironment
 
 
@@ -42,35 +45,24 @@ def get_game_run(matchup: CoupMatchupEnvironment, policy_1: dict, policy_2: dict
     return run
 
 
-def run_experiment():
+def run_experiment(path="results.txt", verbose=False):
     """
     Run and record the result of all matchups
     """
-    pass
+    card_pairs = list(itertools.product(constants.CARDS, constants.CARDS))
+    matchups = list(itertools.product(card_pairs, card_pairs))
+
+    i = 0
+    with open(path, "w") as file:
+        for matchup in matchups:
+            print(f"Solving matchup {i+1}/{len(matchups)}")
+            winner, _, _, _ = run_matchup(matchup[0], matchup[1], verbose=verbose)
+            file.write(f"{matchup[0]},{matchup[1]},{winner}\n")
 
 
 def main():
-    player1_cards = (constants.AMBASSADOR, constants.AMBASSADOR)
-    player2_cards = (constants.ASSASSIN, constants.CONTESSA)
-    # print(CoupMatchupEnvironment.get_enabled_actions((('ambassador', 'ambassador', 6), ('assassin', 'contessa', 4), 2, 0, 0, 0, 0)))
+    run_experiment(verbose=True)
 
-    # matchup = CoupMatchupEnvironment(player1_cards, player2_cards)
-    # state = (('assassin', 'contessa', 8), ('ambassador', 'ambassador', 10), 1, 0, 0, 0, 0)
-    # print(CoupMatchupEnvironment.get_enabled_actions(state))
-    # print(CoupMatchupEnvironment.transition(state, constants.ASSASSINATE))
-    # new_state = CoupMatchupEnvironment.transition(state, constants.ASSASSINATE)
-    # print(CoupMatchupEnvironment.get_enabled_actions(new_state))
-
-    # matchup.solve(verbose=True)
-    # print(state in matchup.get_win_region(1))
-    # print(state in matchup.get_win_region(2))
-    # print(matchup.get_policy(1)[state])
-    # print(matchup.get_policy(2)[state])
-    # print(CoupMatchupEnvironment.get_enabled_actions(state))
-
-    winner, policy_1, policy_2, matchup = run_matchup(player1_cards, player2_cards, verbose=True)
-    print(winner)
-    print(get_game_run(matchup, policy_1, policy_2))
 
 if __name__ == "__main__":
     main()
